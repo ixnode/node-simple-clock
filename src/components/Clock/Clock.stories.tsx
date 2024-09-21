b48a4415fea59286a18a6dce62f2366704d16b65
Björn Hempel <bjoern@hempel.li>
2024-09-22 01:55:26 +0200

    Format analog clock; Add shadow according to given angle; etc.

--- src/components/Clock/Clock.stories.tsx
+++ src/components/Clock/Clock.stories.tsx
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import { Clock } from './Clock';
 
+// @ts-ignore
 const meta: Meta<typeof Clock> = {
     title: 'Components/Clock',
     component: Clock,
@@ -11,29 +12,81 @@ const meta: Meta<typeof Clock> = {
     tags: ['autodocs'],
     argTypes: {
         color: {
-            control: { type: 'select' },
+            control: { type: 'radio' },
+            table: { category: 'Design' },
         },
         size: {
-            control: { type: 'select' },
+            control: { type: 'radio' },
+            table: { category: 'Design' },
+        },
+        showBorder: {
+            control: { type: 'boolean' },
+            table: { category: 'Design' },
         },
         use24HourFormat: {
             control: { type: 'boolean' },
+            table: { category: 'Clock' },
         },
         showTenths: {
             control: { type: 'boolean' },
+            table: { category: 'Clock' },
         },
         isAnalog: {
             control: { type: 'boolean' },
+            table: { category: 'Clock' },
         },
         showDate: {
             control: { type: 'boolean' },
+            table: { category: 'Clock' },
         },
         showTimeZone: {
             control: { type: 'boolean' },
+            table: { category: 'Clock' },
+        },
+        timeZoneType: {
+            control: { type: 'radio' },
+            table: { category: 'Clock' },
         },
         locale: {
             control: { type: 'select' },
-        }
+            table: { category: 'Locale' },
+        },
+        timeZone: {
+            control: { type: 'select' },
+            options: {
+                // @ts-ignore: Ignore TypeScript error for object-based options
+                'UTC': 'UTC',
+                'UTC-12 (Baker Island)': 'Etc/GMT+12',
+                'UTC-11 (American Samoa)': 'Pacific/Pago_Pago',
+                'UTC-10 (Hawaii - Honolulu)': 'Pacific/Honolulu',
+                'UTC-9 (Alaska - Anchorage)': 'America/Anchorage',
+                'UTC-8 (Pacific Time - Los Angeles, Vancouver)': 'America/Los_Angeles',
+                'UTC-7 (Mountain Time - Denver, Phoenix)': 'America/Denver',
+                'UTC-6 (Central Time - Chicago, Mexico City)': 'America/Chicago',
+                'UTC-5 (Eastern Time - New York, Toronto)': 'America/New_York',
+                'UTC-4 (Atlantic Time - Caracas, La Paz)': 'America/Caracas',
+                'UTC-3 (Brasilia, Buenos Aires)': 'America/Sao_Paulo',
+                'UTC-2 (South Georgia & South Sandwich Islands)': 'Atlantic/South_Georgia',
+                'UTC-1 (Azores, Cape Verde)': 'Atlantic/Azores',
+                'UTC+0 (London, Lisbon)': 'Europe/London',
+                'UTC+1 (Berlin, Paris, Madrid)': 'Europe/Berlin',
+                'UTC+2 (Athens, Cairo, Johannesburg)': 'Africa/Johannesburg',
+                'UTC+3 (Moscow, Istanbul, Riyadh)': 'Europe/Moscow',
+                'UTC+4 (Dubai, Baku)': 'Asia/Dubai',
+                'UTC+5 (Pakistan - Karachi)': 'Asia/Karachi',
+                'UTC+5:30 (India - New Delhi, Mumbai)': 'Asia/Kolkata',
+                'UTC+6 (Bangladesh - Dhaka)': 'Asia/Dhaka',
+                'UTC+7 (Bangkok, Hanoi, Jakarta)': 'Asia/Bangkok',
+                'UTC+8 (Beijing, Singapore, Perth)': 'Asia/Shanghai',
+                'UTC+9 (Tokyo, Seoul)': 'Asia/Tokyo',
+                'UTC+10 (Sydney, Melbourne)': 'Australia/Sydney',
+                'UTC+11 (Solomon Islands, New Caledonia)': 'Pacific/Guadalcanal',
+                'UTC+12 (Auckland, Fiji)': 'Pacific/Auckland',
+                'UTC+13 (Tonga, Samoa)': 'Pacific/Tongatapu',
+                'UTC+14 (Line Islands - Kiritimati)': 'Pacific/Kiritimati',
+            },
+            table: { category: 'Locale' },
+        },
     },
 };
 
@@ -44,12 +97,17 @@ export const Default: Story = {
     args: {
         color: 'default',
         size: 'medium',
+        showBorder: false,
+
         use24HourFormat: true,
         showTenths: false,
         isAnalog: false,
         showDate: false,
         showTimeZone: false,
+        timeZoneType: 'short',
+
         locale: 'en-US',
+        timeZone: 'UTC',
     },
 };
 
@@ -57,12 +115,17 @@ export const Analog: Story = {
     args: {
         color: 'default',
         size: 'medium',
+        showBorder: false,
+
         use24HourFormat: true,
         showTenths: false,
         isAnalog: true,
         showDate: false,
         showTimeZone: false,
+        timeZoneType: 'short',
+
         locale: 'en-US',
+        timeZone: 'UTC',
     },
 };
 
@@ -70,12 +133,17 @@ export const Red: Story = {
     args: {
         color: 'red',
         size: 'medium',
+        showBorder: false,
+
         use24HourFormat: true,
         showTenths: false,
         isAnalog: false,
         showDate: false,
         showTimeZone: false,
+        timeZoneType: 'short',
+
         locale: 'en-US',
+        timeZone: 'UTC',
     },
 };
 
@@ -83,12 +151,17 @@ export const Green: Story = {
     args: {
         color: 'green',
         size: 'medium',
+        showBorder: false,
+
         use24HourFormat: true,
         showTenths: false,
         isAnalog: false,
         showDate: false,
         showTimeZone: false,
+        timeZoneType: 'short',
+
         locale: 'en-US',
+        timeZone: 'UTC',
     },
 };
 
@@ -96,11 +169,15 @@ export const Blue: Story = {
     args: {
         color: 'blue',
         size: 'medium',
+        showBorder: false,
+
         use24HourFormat: true,
         showTenths: false,
         isAnalog: false,
         showDate: false,
         showTimeZone: false,
+
         locale: 'en-US',
+        timeZone: 'UTC',
     },
 };
--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -1,12 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import './Clock.scss';
 
 export interface ClockProps {
-    /** What font color to use. */
-    color?: 'default' | 'red' | 'green' | 'blue';
+    /** What font color to use? */
+    color?: 'default'|'red'|'green'|'blue';
 
     /** How large should the clock be? */
-    size?: 'small' | 'medium' | 'large';
+    size?: 'small'|'medium'|'large';
+
+    /** Show border? */
+    showBorder?: boolean;
 
     /** Use 24 hour format? true for 24h, false for AM/PM */
     use24HourFormat?: boolean;
@@ -20,27 +23,92 @@ export interface ClockProps {
     /** Show date? */
     showDate?: boolean;
 
-    /** Should a time zone be displayed? */
+    /** Should a timezone be displayed? */
     showTimeZone?: boolean,
+
+    /** Which type of timezone should be displayed? */
+    timeZoneType?: 'short'|'long';
 
     /** Which locale should be displayed? */
     locale?: 'en-US'|'en-GB'|'de-DE'|'es-ES'|'fr-FR'|'it-IT'|'nl-NL'|'pl-PL'|'pt-BR'|'ru-RU'|'sv-SE'|'zh-CN'|'zh-TW';
+
+    /** Which time zone should be displayed? */
+    timeZone?: string,
 }
+
+/**
+ * Calculates the shadow offset according to the given angle.
+ *
+ * @param angle
+ * @param hand
+ */
+const calculateShadowOffsets = (angle: number, hand: boolean = false) => {
+    const angleShadow = -angle - 45;
+
+    const radians = angleShadow * (Math.PI / 180);
+    const offsetX = -Math.floor(1.5 * 100 * Math.sin(radians)) / 100;
+    const offsetY = Math.floor(1.5 * 100 * Math.cos(radians)) / 100;
+
+    return { offsetX, offsetY };
+}
+
+/**
+ * Updates the shadow offsets for the given hand.
+ *
+ * @param refObject
+ * @param angle
+ */
+const updateShadow = (refObject: React.RefObject<HTMLDivElement>, angle: number) => {
+    const { offsetX, offsetY} = calculateShadowOffsets(angle, true);
+    const boxShadowSeconds = shadowTemplate
+        .replace("${offsetX}", offsetX.toString())
+        .replace("${offsetY}", offsetY.toString());
+
+    if (refObject.current) {
+        refObject.current.style.boxShadow = boxShadowSeconds;
+    }
+}
+
+const shadowTemplate = '${offsetX}em ${offsetY}em 1.5em rgba(0, 0, 0, 0.5)';
 
 export const Clock: React.FC<ClockProps> = ({
     color = 'default',
     size = 'medium',
+    showBorder = false,
     use24HourFormat = true,
     showTenths = false,
     isAnalog = false,
     showDate = false,
     showTimeZone = false,
+    timeZoneType = 'short',
     locale = 'en-US',
+    timeZone = 'UTC',
 }) => {
     const [time, setTime] = useState(new Date());
 
+    const hourHandRef: React.RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
+    const minuteHandRef = useRef<HTMLDivElement>(null);
+    const secondHandRef = useRef<HTMLDivElement>(null);
+
     useEffect(() => {
-        const timer = setInterval(() => setTime(new Date()), showTenths ? 100 : 1000);
+        const timer = setInterval(() => {
+            const newTime = new Date();
+            const seconds = newTime.getSeconds();
+
+            if (seconds === 1 && secondHandRef.current) {
+                secondHandRef.current.style.transition = 'none';
+                secondHandRef.current.style.transform = `rotate(0deg)`;
+
+            }
+
+            setTimeout(() => {
+                if (seconds === 1 && secondHandRef.current) {
+                    secondHandRef.current.style.transition = '';
+                }
+                setTime(newTime);
+            }, 100);
+        }, showTenths ? 100 : 1000);
+
         return () => clearInterval(timer);
     }, [showTenths]);
 
@@ -55,58 +123,66 @@ export const Clock: React.FC<ClockProps> = ({
         minute: '2-digit',
         second: '2-digit',
         hour12: !use24HourFormat, /* Switch between 12-hour and 24-hour format */
+        timeZone: timeZone, /* Time zone to display */
     }).format(time);
 
     const formattedTenths = showTenths ? Math.floor(time.getMilliseconds() / 100) : null;
 
     /* Time zone formatting */
     const formattedTimeZone = showTimeZone
-        ? new Intl.DateTimeFormat(locale, { timeZoneName: 'short' }).format(time).split(' ')[1]
+        ? new Intl.DateTimeFormat(locale, { timeZone: timeZone, timeZoneName: timeZoneType }).format(time).split(', ')[1]
         : '';
 
     /* Analog clock - Calculation of rotation angles for hands */
-    const hours = time.getHours();
-    const minutes = time.getMinutes();
-    const seconds = time.getSeconds();
+    const hours = parseInt(new Intl.DateTimeFormat(locale, { hour: 'numeric', hour12: true, timeZone }).format(time));
+    const minutes = parseInt(new Intl.DateTimeFormat(locale, { minute: 'numeric', timeZone }).format(time));
+    const seconds = parseInt(new Intl.DateTimeFormat(locale, { second: 'numeric', timeZone }).format(time));
 
-    const hourDegrees = ((hours % 12) / 12) * 360 + (minutes / 60) * 30 - 90;
-    const minuteDegrees = (minutes / 60) * 360 - 90;
-    const secondDegrees = (seconds / 60) * 360 - 90;
+    const hourAngle = (hours / 12) * 360 + (minutes / 60) * 30;
+    const minuteAngle = (minutes / 60) * 360;
+    let secondAngle = (seconds === 0 ? 360 : (seconds / 60) * 360);
 
-    // Create hour marks
+    /* Create hour marks */
     const marks = Array.from({ length: 12 }).map((_, index) => {
         const isLongMark = [0, 3, 6, 9].includes(index); // Long marks for 12, 3, 6, 9
-        const markClass = isLongMark ? 'mark mark-long' : 'mark mark-short';
-        const rotation = `rotate(${index * 30}deg)`; // 30 degrees per hour mark
-        return <div key={index} className={markClass} style={{ transform: rotation }}>
-            <div className="mark-line" />
+        const markClass = 'mark' + (isLongMark ? ' mark-long' : ' mark-short') + ' mark-' + index;
+        const angle = index * 30;
+        const transform = `rotate(${angle}deg)`; // 30 degrees per hour mark
+        const { offsetX, offsetY } = calculateShadowOffsets(angle);
+        const boxShadow = shadowTemplate
+            .replace("${offsetX}", offsetX.toString())
+            .replace("${offsetY}", offsetY.toString());
+
+        return <div key={index} className={markClass} style={{ transform }}>
+            <div className="mark-line" style={{ boxShadow }} />
         </div>;
     });
 
+    updateShadow(hourHandRef, hourAngle);
+    updateShadow(minuteHandRef, minuteAngle);
+    updateShadow(secondHandRef, secondAngle);
+
     const hourHandStyle = {
-        transform: `rotate(${hourDegrees}deg)`,
-        transition: hours === 0 ? 'none' : 'transform 0.05s ease-in-out', /* Disable transition at 0 minutes. */
+        transform: `rotate(${hourAngle}deg)`,
     };
 
     const minuteHandStyle = {
-        transform: `rotate(${minuteDegrees}deg)`,
-        transition: minutes === 0 ? 'none' : 'transform 0.05s ease-in-out', /* Disable transition at 0 minutes. */
+        transform: `rotate(${minuteAngle}deg)`,
     };
 
     const secondHandStyle = {
-        transform: `rotate(${secondDegrees}deg)`,
-        transition: seconds === 0 ? 'none' : 'transform 0.05s ease-in-out', /* Disable transition at 0 seconds. */
+        transform: `rotate(${secondAngle}deg)`,
     };
 
     return (
-        <div className={'clock' + ' ' + color + ' ' + size}>
+        <div className={'clock' + ' ' + color + ' ' + size + ' ' + (showBorder ? 'with-border' : 'no-border')}>
             {isAnalog ? (
                 <div className="analog-clock">
                     <div className="clock-face">
                         {marks}
-                        <div className="hand hour-hand" style={hourHandStyle} />
-                        <div className="hand minute-hand" style={minuteHandStyle} />
-                        <div className="hand second-hand" style={secondHandStyle} />
+                        <div className="hand hour-hand" style={hourHandStyle} ref={hourHandRef} />
+                        <div className="hand minute-hand" style={minuteHandStyle} ref={minuteHandRef} />
+                        <div className="hand second-hand" style={secondHandStyle} ref={secondHandRef} />
                         <div className="point">
                             <div className="point-marker" />
                         </div>
@@ -117,7 +193,7 @@ export const Clock: React.FC<ClockProps> = ({
                     {showDate && <>{formattedDate} - </>}
                     {formattedTime}
                     {showTenths && <span className={'tents'}>.{formattedTenths}</span>}
-                    {showTimeZone && <> {formattedTimeZone}</>}
+                    {showTimeZone && <>, {formattedTimeZone}</>}
                 </>
             )}
         </div>
