2e5eab97c9a8c97a9077e3f66dd7649980386166
Björn Hempel <bjoern@hempel.li>
2024-12-21 16:36:50 +0100

    Rollup refactoring

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,22 +1,25 @@
 import sass from 'rollup-plugin-sass';
 import typescript from '@rollup/plugin-typescript';
-import fs from 'fs';
 
 export default {
     input: 'src/index.ts',
-    output: {
-        file: 'dist/index.js',
-        format: 'cjs',
-    },
+    output: [
+        {
+            file: 'dist/index.js',
+            format: 'esm',
+        },
+        {
+            file: 'dist/index.cjs',
+            format: 'cjs',
+        }
+    ],
     external: ['react', 'react-dom', 'react/jsx-runtime'],
     plugins: [
         typescript({
             tsconfig: './tsconfig.json',
         }),
         sass({
-            output(styles) {
-                fs.writeFileSync('dist/styles.css', styles); // Schreibe die CSS-Datei
-            },
+            output: 'dist/styles.css',
         }),
     ],
 }
